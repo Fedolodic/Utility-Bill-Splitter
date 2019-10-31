b7c8998af1ebbb5ed9926e675c21b1a170bfde42
David Martinez <david.mtz00@hotmail.com>
2019-10-30 22:45:39 -0500

    Added transition functionality

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,42 @@
-import React, {Fragment} from 'react';
+import React, {Fragment,useState} from 'react';
 import TotalsTable from '../components/TotalsTable'
 import SplitsTable from '../components/SplitsTable'
 import MonthTable from '../components/MonthTable'
 import {Row, Col} from 'reactstrap'
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import {animated, useTransition} from 'react-spring';
 
-export default function Index() {
-    return (
+const Index = () => {
+    const [on, toggle] = useState(false);
+
+    const transition = useTransition(on, null, {
+        initial: { transform: 'translate3d(0, 0%,0)'},
+        from:    { transform: 'translate3d(0, 80%,0)'},
+        enter:   { transform: 'translate3d(0, 0%,0)'},
+        leave:   { transform: 'translate3d(0, -90%,0)'}
+    });
+
+    return(
         <Fragment>
-            <ReactCSSTransitionGroup
-                component="div"
-                transitionName="TabsAnimation"
-                transitionAppear={true}
-                transitionAppearTimeout={3000}
-                transitionEnter={false}
-                transitionLeave={false}
-            >
-                <div>
-                <Row>
-                    <Col lg="6">
-                        <TotalsTable/>
-                    </Col>
-                    <Col lg="6">
-                        <SplitsTable/>
-                    </Col>
-                    <Col lg="6">
-                        <MonthTable/>
-                    </Col>
-                </Row>
-                </div>
-            </ReactCSSTransitionGroup>
+            <div>
+                {transition.map(({ item, props }) => (
+                    item && <animated.div style={props}>
+                        <Row>
+                            <Col lg="6">
+                                <TotalsTable/>
+                            </Col>
+                            <Col lg="6">
+                                <SplitsTable/>
+                            </Col>
+                            <Col lg="6">
+                                <MonthTable/>
+                            </Col>
+                        </Row>
+                    </animated.div>
+                ))}
+                <button onClick={() => toggle(!on)}>Change</button>
+            </div>
         </Fragment>
-    )
-}
+    );
+};
+
+export default Index;
