63a6566d097b5a3676266b9a717f7aa61e27b38e
David Martinez <david.mtz00@hotmail.com>
2019-10-30 23:24:05 -0500

    Improved transition functionality

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,23 @@ import {Row, Col} from 'reactstrap'
 import {animated, useTransition} from 'react-spring';
 
 const Index = () => {
+
     const [on, toggle] = useState(false);
 
-    const transition = useTransition(on, null, {
+    const transitions = useTransition(on, null,{
         initial: { transform: 'translate3d(0, 0%,0)'},
-        from:    { transform: 'translate3d(0, 80%,0)'},
-        enter:   { transform: 'translate3d(0, 0%,0)'},
-        leave:   { transform: 'translate3d(0, -90%,0)'}
+        from: { transform: 'translate3d(0, 80%,0)'},
+        enter: { transform: 'translate3d(0, 0%,0)'},
+        leave: { transform: 'translate3d(0, -90%,0)'}
     });
 
     return(
         <Fragment>
             <div>
-                {transition.map(({ item, props }) => (
-                    item && <animated.div style={props}>
+                {transitions.map(({ item, key, props}) => (
+                    item && <animated.div
+                        style={props}
+                        key={key}>
                         <Row>
                             <Col lg="6">
                                 <TotalsTable/>
@@ -31,12 +34,28 @@ const Index = () => {
                                 <MonthTable/>
                             </Col>
                         </Row>
-                    </animated.div>
-                ))}
-                <button onClick={() => toggle(!on)}>Change</button>
+                    </animated.div>))}
             </div>
+            <button onClick={() => toggle(!on)}>Change</button>
         </Fragment>
     );
 };
 
 export default Index;
+
+/*
+transition.map(({ item, props }) => (
+    item && <animated.div style={props}>
+        <Row>
+            <Col lg="6">
+                <TotalsTable/>
+            </Col>
+            <Col lg="6">
+                <SplitsTable/>
+            </Col>
+            <Col lg="6">
+                <MonthTable/>
+            </Col>
+        </Row>
+    </animated.div>
+))*/
