4cbbc6665c6231ecae5d337fa66ce4f1d72be84e
David Martinez <david.mtz00@hotmail.com>
2020-05-24 18:29:20 -0500

    Abstract <TotalsTable/> component

--- components/TotalsTable/TotalsTable.js
+++ components/TotalsTable/TotalsTable.js
@@ -3,6 +3,18 @@ import {Table} from 'reactstrap';
 import {allBills} from "../BillsList";
 
 export default class TotalsTable extends Component {
+    displayTotals() {
+        return Object.entries(allBills).map(([month, bill]) => {
+            return (
+                <tr>
+                    <th scope="row">{month}</th>
+                    <td>${(bill.electricity + bill.water + bill.internet + bill.gas).toFixed(2)}</td>
+                    <td>${((bill.electricity + bill.water + bill.internet + bill.gas)/4).toFixed(2)}</td>
+                </tr>
+            )
+        })
+    }
+
     render() {
         return (
             <Table hover className="mb-0">
@@ -14,46 +26,7 @@ export default class TotalsTable extends Component {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">September</th>
-                    <td>$411.78</td>
-                    <td>$143.53</td>
-                </tr>
-                <tr>
-                    <th scope="row">October</th>
-                    <td>$299.56</td>
-                    <td>$74.89</td>
-                </tr>
-                <tr>
-                    <th scope="row">November</th>
-                    <td>$320.39</td>
-                    <td>$80.10</td>
-                </tr>
-                <tr>
-                    <th scope="row">December</th>
-                    <td>$330.17</td>
-                    <td>$82.54</td>
-                </tr>
-                <tr>
-                    <th scope="row">January</th>
-                    <td>$243.14</td>
-                    <td>$60.79</td>
-                </tr>
-                <tr>
-                    <th scope="row">February</th>
-                    <td>$230.14</td>
-                    <td>$57.54</td>
-                </tr>
-                <tr>
-                    <th scope="row">March</th>
-                    <td>$259.12</td>
-                    <td>$64.79</td>
-                </tr>
-                <tr>
-                    <th scope="row">April</th>
-                    <td>$205.85</td>
-                    <td>$51.46</td>
-                </tr>
+                {this.displayTotals()}
                 </tbody>
             </Table>
         );
